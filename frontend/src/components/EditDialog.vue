1bcba9895ff3eee2574142f8531c3f80a36dca09
CodePaluer <2205036781@qq/com>
2025-06-13 13:51:33 +0800

    前端-editdialog-bugfix

--- frontend/src/components/EditDialog.vue
+++ frontend/src/components/EditDialog.vue
@@ -1,15 +1,20 @@
 <template>
-        <el-dialog v-model="visible" :title="title" width="500">
-            <el-form :model="model">
-                <el-form-item v-for="field in fields" :key="field.prop" :label="field.label">
-                    <component :is="field.component" v-model="model[field.prop]" v-bind="field.props" />
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <el-button @click="visible = false">取消</el-button>
-                <el-button type="primary" @click="$emit('submit')">确认修改</el-button>
-            </template>
-        </el-dialog>
+    <el-dialog v-model="visible" :title="title" width="500">
+        <el-form :model="model">
+            <el-form-item v-for="field in fields" :key="field.prop" :label="field.label">
+                <component :is="field.component" v-model="model[field.prop]" v-bind="field.props">
+                    <template v-if="field.options" #default>
+                        <el-option v-for="option in field.options" :key="option.value" :label="option.label"
+                            :value="option.value" />
+                    </template>
+                </component>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <el-button @click="visible = false">取消</el-button>
+            <el-button type="primary" @click="$emit('submit')">确认修改</el-button>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup>
