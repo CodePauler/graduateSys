26e5c264c5de475bd07a10a773b0504460e1602b
EternalMajesty <2110612912@qq.com>
2025-06-16 14:24:10 +0800

    修改部分SQL语句顺序

--- dhugraduate.sql
+++ dhugraduate.sql
@@ -248,28 +248,14 @@ DROP VIEW IF EXISTS `company_job_summary`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `company_job_summary` AS select `c`.`company_id` AS `company_id`,`c`.`company_name` AS `company_name`,count(`j`.`job_id`) AS `job_count`,sum(`j`.`demand_number`) AS `total_demand`,sum((select count(0) from `employment` `e` where ((`e`.`job_id` = `j`.`job_id`) and (`e`.`status` = '已录用')))) AS `total_hired` from (`company` `c` left join `job` `j` on((`c`.`company_id` = `j`.`company_id`))) group by `c`.`company_id`,`c`.`company_name`;
 
 -- ----------------------------
--- View structure for department_info
--- ----------------------------
-DROP VIEW IF EXISTS `department_info`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `department_info` AS select `d`.`id` AS `department_id`,`d`.`name` AS `department_name`,count(`s`.`student_id`) AS `total_students`,sum((`s`.`employment_status` = '就业')) AS `employed_students`,round(ifnull((sum((`s`.`employment_status` = '就业')) / nullif(count(`s`.`student_id`),0)),0),4) AS `employment_rate` from ((`department` `d` left join `major` `m` on((`d`.`id` = `m`.`department_id`))) left join `student_info` `s` on((`m`.`id` = `s`.`major_id`))) group by `d`.`id`,`d`.`name`;
-
--- ----------------------------
--- View structure for global_info
--- ----------------------------
-DROP VIEW IF EXISTS `global_info`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `global_info` AS select (select count(0) from `student`) AS `total_students`,(select count(0) from `student_info` where (`student_info`.`employment_status` = '就业')) AS `employed_students`,(select count(0) from `student_info` where (`student_info`.`employment_status` = '待业')) AS `unemployed_students`,(select count(0) from `company`) AS `company_count`,(select sum(`job`.`demand_number`) from `job`) AS `total_job_demand`,(select sum(`job_info`.`hired_number`) from `job_info`) AS `total_hired`,(select count(0) from `job`) AS `job_count`,(select count(0) from `employment` where (`employment`.`status` = '已录用')) AS `total_employment_records`,(select count(0) from `announcement`) AS `announcement_count`;
-
--- ----------------------------
 -- View structure for job_info
 -- ----------------------------
 DROP VIEW IF EXISTS `job_info`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `job_info` AS select `j`.`job_id` AS `job_id`,`j`.`title` AS `title`,`j`.`type_id` AS `type_id`,`j`.`demand_number` AS `demand_number`,`j`.`company_id` AS `company_id`,`j`.`status` AS `status`,`j`.`description` AS `description`,(select count(0) from `employment` `e` where ((`e`.`job_id` = `j`.`job_id`) and (`e`.`status` = '已录用'))) AS `hired_number` from `job` `j`;
 
--- ----------------------------
--- View structure for student_employment_summary
--- ----------------------------
-DROP VIEW IF EXISTS `student_employment_summary`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `student_employment_summary` AS select `s`.`student_id` AS `student_id`,`u`.`name` AS `student_name`,`s`.`employment_status` AS `employment_status`,count(`e`.`employment_id`) AS `total_applications`,sum((`e`.`status` = '已录用')) AS `hired_count` from ((`student_info` `s` left join `user` `u` on((`s`.`user_id` = `u`.`id`))) left join `employment` `e` on((`s`.`student_id` = `e`.`student_id`))) group by `s`.`student_id`,`u`.`name`,`s`.`employment_status`;
+
+
+
 
 -- ----------------------------
 -- View structure for student_info
@@ -278,3 +264,23 @@ DROP VIEW IF EXISTS `student_info`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `student_info` AS select `s`.`student_id` AS `student_id`,`s`.`user_id` AS `user_id`,`s`.`major_id` AS `major_id`,`s`.`graduation_year` AS `graduation_year`,`s`.`resume_url` AS `resume_url`,(case when exists(select 1 from `employment` `e` where ((`e`.`student_id` = `s`.`student_id`) and (`e`.`status` = '已录用'))) then '就业' else '待业' end) AS `employment_status` from `student` `s`;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- View structure for student_employment_summary
+-- ----------------------------
+DROP VIEW IF EXISTS `student_employment_summary`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `student_employment_summary` AS select `s`.`student_id` AS `student_id`,`u`.`name` AS `student_name`,`s`.`employment_status` AS `employment_status`,count(`e`.`employment_id`) AS `total_applications`,sum((`e`.`status` = '已录用')) AS `hired_count` from ((`student_info` `s` left join `user` `u` on((`s`.`user_id` = `u`.`id`))) left join `employment` `e` on((`s`.`student_id` = `e`.`student_id`))) group by `s`.`student_id`,`u`.`name`,`s`.`employment_status`;
+
+
+-- ----------------------------
+-- View structure for department_info
+-- ----------------------------
+DROP VIEW IF EXISTS `department_info`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `department_info` AS select `d`.`id` AS `department_id`,`d`.`name` AS `department_name`,count(`s`.`student_id`) AS `total_students`,sum((`s`.`employment_status` = '就业')) AS `employed_students`,round(ifnull((sum((`s`.`employment_status` = '就业')) / nullif(count(`s`.`student_id`),0)),0),4) AS `employment_rate` from ((`department` `d` left join `major` `m` on((`d`.`id` = `m`.`department_id`))) left join `student_info` `s` on((`m`.`id` = `s`.`major_id`))) group by `d`.`id`,`d`.`name`;
+
+
+-- ----------------------------
+-- View structure for global_info
+-- ----------------------------
+DROP VIEW IF EXISTS `global_info`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `global_info` AS select (select count(0) from `student`) AS `total_students`,(select count(0) from `student_info` where (`student_info`.`employment_status` = '就业')) AS `employed_students`,(select count(0) from `student_info` where (`student_info`.`employment_status` = '待业')) AS `unemployed_students`,(select count(0) from `company`) AS `company_count`,(select sum(`job`.`demand_number`) from `job`) AS `total_job_demand`,(select sum(`job_info`.`hired_number`) from `job_info`) AS `total_hired`,(select count(0) from `job`) AS `job_count`,(select count(0) from `employment` where (`employment`.`status` = '已录用')) AS `total_employment_records`,(select count(0) from `announcement`) AS `announcement_count`;
